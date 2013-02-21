bdaa20c730be2d7fcb36b925f89960197dc4d9e8
unknown <webpro@me.com>
2013-02-21 11:33:45 +0200

    build configuration and base integration with feed

---
+++ source/javascripts/app.js
@@ -0,0 +1,11 @@
+//Third Parties
+//=require vendor/jquery-1.9.1.min
+//=require vendor/jquery-migrate-1.1.1.min
+//require vendor/jquery-1.6.4.min
+//=require vendor/jquery.mobile-1.1.0.min
+//=require plugins
+//=require vendor/handlebars
+//=require vendor/add2home
+
+
+//=require RSSFeedContainer
